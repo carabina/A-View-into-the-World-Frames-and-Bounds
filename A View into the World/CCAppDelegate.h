ac1dc7b744b1ad57fb36dee03e203b9a50e39e8d
Eliot Arntz <arntzel@gmail.com>
2013-11-07 12:48:46 +0100

    Initial Commit

---
+++ A View into the World/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  A View into the World
+//
+//  Created by Eliot Arntz on 11/7/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ A View into the World/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  A View into the World
+//
+//  Created by Eliot Arntz on 11/7/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
